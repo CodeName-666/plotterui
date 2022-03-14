cfb74903ab653b4ef3fd7c6a377895775bcb62f1
Christof Seidel <seidelchristof@googlemail.com>
2022-03-14 13:43:12 +0100

    update

---
+++ imports/PlotterUi/Timer.js
@@ -0,0 +1,25 @@
+.pragma library
+
+/*******************************************************************
+ * FUNCTION
+ ******************************************************************/
+function Timer(parent, interval, start = false, repeat = false, callback = undefined) {
+    if (setup_done_status) {
+
+        var cTimer = Qt.createQmlObject(' import QtQuick 2.15; Timer {}',parent);
+
+        cTimer.interval = interval
+        cTimer.repeat = repeat
+
+        if (callback !== undefined) {
+            cTimer.triggered.connect(callback)
+        }
+
+        if (start) {
+            cTimer.start()
+        }
+
+        return cTimer
+    }
+    return None
+}
